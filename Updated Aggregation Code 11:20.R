0eef7c0eeed439c7b4fc58caf49b22f878c86e48
lauracg2 <gray.laura97@gmail.com>
2020-11-09 10:05:29 -0600

    Got the for loop working!

--- Updated Aggregation Code 11:20.R
+++ Updated Aggregation Code 11:20.R
@@ -6,12 +6,6 @@ library(dplyr)
 workdir <- "/Users/lauragray/Downloads/Research/GRDC Data/comp_runoff_hdeg"
 setwd(workdir)
 
-Jan86 <- read.ascii.grid("comp_runoff_hd_198601.asc", return.header=FALSE, print = 0, 
-                         nodata.values = c(), at.once = TRUE, na.strings = "NA")
-Jan86 <- t(Jan86)
-Jan86 <- Jan86[,order(ncol(Jan86):1)]
-Jan86 <- Jan86 %>% dplyr::na_if(-88)
-
 library(ncdf4)
 workdir <- "/Users/lauragray"
 setwd(workdir)
@@ -20,19 +14,6 @@ surface_runoff_array <- ncvar_get(fi, "QRUNOFF")
 
 CJan86 <- surface_runoff_array[, , 1633]
 CJan86 <- CJan86*3600*24*31
-
-#################### This for loop does not work so that's fun ##############################
-data <- array(data = NA, dim = c(360, 720))
-data <- data.frame(data)
-for (year in 1986:1995){
-  for (month in 1:12) {
-    filename <- paste0("comp_runoff_hd_",sprintf("%04d",year),sprintf("%02d",month),".asc")
-    print(filename)
-    data[[i]] <- read.ascii.grid(filename, return.header=FALSE, print = 0,
-                    nodata.values = c(), at.once = TRUE, na.strings = "NA")
-    i <- i+1
-  }
-}
 
 ######## This for loop does work! But all it does is paste the names in a dataframe ##############################
 grdcnames <- NULL
@@ -44,21 +25,21 @@ for (year in 1986:1995){
   }
 }
 
-
-####### Trying to get this for loop to read in all of the ascii files at once, does not work currently ################
-
-GRDC_m <- array(NA, c(360, 720, 120))
+GRDC_m <- array(NA, c(720, 360, 120))
 for (i in 1:length(grdcnames)) {
   grdcdata <- read.ascii.grid(grdcnames[i, 1], return.header=FALSE, print = 0,
               nodata.values = c(), at.once = TRUE, na.strings = "NA")
+  grdcdata <- t(grdcdata)
+  grdcdata <- grdcdata[,order(ncol(grdcdata):1)]
+  grdcdata <- grdcdata %>% dplyr::na_if(-88)
   GRDC_m[ , , i] <- grdcdata
 }
 
-
 ############## Projected for loop once I get the previous one working ##################
 #how I think the overall aggregation for loop should look (something like this ?)
-for (file in GRDCfiles) {
-  GRDC_agg[file]<-array(NA,c(288,192,120))
+for (k in 1:120) {
+  cat(paste("********* ",as.character(k)," *******"),sep='\n')
+  GRDC_agg <- array(NA,c(288,192,120))
   dLatCESM<-0.9424; dLonCESM<-1.25;
   for (i in 1:288) {
     for (j in 1:192) {
@@ -67,10 +48,13 @@ for (file in GRDCfiles) {
       edgeLon<-c(lon2D[i,j]-dLonCESM/2,lon2D[i,j]+dLonCESM/2)
       isIn <- (GRDClat2D>edgeLat[1] & GRDClat2D<edgeLat[2] & GRDClon2D>edgeLon[1] & GRDClon2D<edgeLon[2])
       sum(sum(isIn,na.rm = T))
-      GRDC_agg[i,j]<-mean(GRDC_agg[file[isIn]], na.rm = T)
+      GRDC_agg[i,j,k]<-mean(GRDC_m[,,k][isIn], na.rm = T)
     }
   }
 }
+
+save(list = c("GRDC_m", "GRDC_agg"), file = "GRDC_Aggregation.RData")
+load("GRDC_Aggregation.RData")
 
 ############### Original aggregation for one file for reference ####################
 
